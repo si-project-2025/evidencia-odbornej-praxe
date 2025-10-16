fdada60577851d8270882681891b900d7af02863
Nathaniel Fruni <fruninathan@gmail.com>
2025-10-16 20:41:49 +0200

    login page frontend

---
+++ si_project_2025_fe/src/components/form/Input.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  import { defineProps, defineEmits } from 'vue'
+
+  defineProps({
+    modelValue: { type: String, required: true },
+    label: { type: String, default: '' },
+    id: { type: String, default: '' },
+    type: { type: String, default: 'text' },
+    placeholder: { type: String, default: '' },
+    required: { type: Boolean, default: true },
+  })
+
+  const emits = defineEmits(['update:modelValue'])
+
+  const updateValue = (event: Event) => {
+    const target = event.target as HTMLInputElement
+    emits('update:modelValue', target.value)
+  }
+</script>
+
+<template>
+  <div class="w-full">
+    <label :for="id" class="font-semibold">{{ label }}</label>
+    <input
+      :id="id"
+      :type="type"
+      :placeholder="placeholder"
+      :required="required"
+      class="w-full input"
+      :value="modelValue"
+      @input="updateValue"
+    />
+  </div>
+</template>
--- si_project_2025_fe/src/router/index.ts
+++ si_project_2025_fe/src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
+import LoginPage from '@/pages/LoginPage.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: HomePage
+    component: HomePage,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginPage,
   },
 ]
 
